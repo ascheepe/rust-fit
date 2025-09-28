fd5ad65e19bf6e9a2bff4db595fa6476ca7678b4
Axel Scheepers <no@spam.please>
2025-09-28 20:37:58 +0200

    slow progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error;
 use std::fmt;
 use std::fs;
 use std::io;
@@ -11,6 +12,8 @@ struct Config {
     link_destination: PathBuf,
     bucket_capacity: u64,
     recursive: bool,
+    dry_run: bool,
+    verbose: bool,
 }
 
 struct FileInfo {
@@ -61,9 +64,22 @@ impl<'a> Bucket<'a> {
 
         false
     }
+
+    fn link(self, verbose: bool) {
+        for file in self.contents {
+            let mut target = self.path.clone();
+            target.push(file.path.clone());
+            let dir = target.parent().unwrap();
+
+            if verbose {
+                println!("{} -> {}", file.path.display(), target.display());
+                println!("dir={}", dir.display());
+            }
+        }
+    }
 }
 
-pub struct HumanNumber(pub u64);
+struct HumanNumber(pub u64);
 
 impl FromStr for HumanNumber {
     type Err = String;
@@ -121,25 +137,19 @@ fn collect_files(
         }
 
         if meta.is_file() {
-            let path = entry.path();
-            let size = meta.len();
-            if size > max_size {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    format!("Can never fit {} ({}).", path.display(), HumanNumber(size)),
-                ));
-            }
-            files.push(FileInfo { path, size });
+            files.push(FileInfo { path: entry.path(), size: meta.len() });
         }
     }
     Ok(())
 }
 
 fn make_config() -> io::Result<Config> {
-    let mut source_directory: PathBuf = PathBuf::new();
-    let mut link_destination: PathBuf = PathBuf::new();
+    let mut source_directory: PathBuf = PathBuf::from(".");
+    let mut link_destination: PathBuf = PathBuf::from("part");
     let mut bucket_capacity: u64 = HumanNumber::from_str("15M").unwrap().0;
     let mut recursive: bool = false;
+    let mut dry_run: bool = false;
+    let mut verbose: bool = false;
 
     let args: Vec<String> = env::args().collect();
     for arg in args {
@@ -149,7 +159,7 @@ fn make_config() -> io::Result<Config> {
             }
         } else if arg.starts_with("--link-destination=") {
             if let Some(val) = arg.strip_prefix("--link-destination=") {
-                link_destination = PathBuf::from(if !val.is_empty() { val } else { "." });
+                link_destination = PathBuf::from(if !val.is_empty() { val } else { "part" });
             }
         } else if arg.starts_with("--bucket-capacity=") {
             if let Some(val) = arg.strip_prefix("--bucket-capacity=") {
@@ -160,6 +170,10 @@ fn make_config() -> io::Result<Config> {
             }
         } else if arg == "--recursive" {
             recursive = true;
+        } else if arg == "--dry-run" {
+            dry_run = true;
+        } else if arg == "--verbose" {
+            verbose = true;
         }
     }
 
@@ -168,6 +182,8 @@ fn make_config() -> io::Result<Config> {
         link_destination: link_destination,
         bucket_capacity: bucket_capacity,
         recursive: recursive,
+        dry_run: dry_run,
+        verbose: verbose,
     })
 }
 
@@ -180,10 +196,9 @@ fn numbered_dir_namer(prefix: &str) -> impl FnMut() -> PathBuf {
     }
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn error::Error>>{
     let cfg = make_config()?;
 
-    println!("{:?}", cfg);
     let mut files: Vec<FileInfo> = Vec::new();
     collect_files(
         &cfg.source_directory,
@@ -191,7 +206,15 @@ fn main() -> io::Result<()> {
         cfg.bucket_capacity,
         &mut files,
     )?;
+
+    if files.len() < 1 {
+        return Err(format!("No files found in {}.", cfg.source_directory.display()).into());
+    }
+
     files.sort_by(|a, b| b.size.cmp(&a.size));
+    if files[0].size > cfg.bucket_capacity {
+        return Err(format!("Can never fit {} ({}).", files[0].path.display(), HumanNumber(files[0].size)).into());
+    }
 
     let mut buckets: Vec<Bucket> = Vec::new();
     let mut new_bucket_name = numbered_dir_namer(cfg.link_destination.to_str().unwrap());
@@ -216,7 +239,11 @@ fn main() -> io::Result<()> {
     }
 
     for bucket in buckets {
-        println!("{}", bucket);
+        if cfg.dry_run {
+            println!("{}", bucket);
+        } else {
+            bucket.link(cfg.verbose);
+        }
     }
 
     Ok(())
